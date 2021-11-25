e922bce74f193b993b1e070f8c34fc88a26fbeb5
SimonNass <57091081+SimonNass@users.noreply.github.com>
2021-11-25 13:36:37 +0100

    Decoder Ein/Ausgangs-Namen Konvertiert

--- VHDL/Decoder.vhd
+++ VHDL/Decoder.vhd
@@ -33,30 +33,30 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Decoder is
   Port ( instruction_i: in std_logic_vector(17 downto 0); -- input signals
-         Clk_i: in std_logic;
+         clk_i: in std_logic;
          reset_i: in std_logic;
          
          constant_kk_o: out std_logic_vector(7 downto 0); -- output signals -- Constant
          constant_aaa_o: out std_logic_vector(11 downto 0);
          
-         MUX_i_o_select_o: out std_logic; -- i/o signals
+         mux_i_o_select_o: out std_logic; -- i/o signals
          
          register_X_ID_o: out std_logic_vector(3 downto 0); --register signals
          register_Y_ID_o: out std_logic_vector(3 downto 0);
-         MUX_register_select_o: out std_logic_vector(1 downto 0);
+         mux_register_select_o: out std_logic_vector(1 downto 0);
          
-         MUX_ALU_select_o: out std_logic; --ALU signals
-         ALU_select_o: out std_logic_vector(5 downto 0);
+         mux_ALU_select_o: out std_logic; --ALU signals
+         sALU_select_o: out std_logic_vector(5 downto 0);
          
          
-         MUX_stack_select_o: out std_logic; --stack signals
-         Stack_enable_o: out std_logic;
+         mux_stack_select_o: out std_logic; --stack signals
+         stack_enable_o: out std_logic;
          
-         MUX_PC_select_o: out std_logic_vector(1 downto 0);-- PC signals
-         OR_PC_o: out std_logic;
+         mux_PC_select_o: out std_logic_vector(1 downto 0);-- PC signals
+         or_PC_o: out std_logic;
          
          store_fetch_o: out std_logic; -- RAM signals
-         RAM_enable_o: out std_logic);
+         sRAM_enable_o: out std_logic);
 end Decoder;
 
 architecture Behavioral of Decoder is
