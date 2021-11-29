da0cd833d9ba1b06445d7920b40fdf71b7dab300
Jannik Graef <jannik.graef@gmail.com>
2021-11-29 16:28:33 +0100

    changed data from signal to variable

--- VHDL/registers/registers.vhd
+++ VHDL/registers/registers.vhd
@@ -47,31 +47,31 @@ entity registers is
 end registers;
 
 architecture Behavioral of registers is  
+  
 
-  signal data  : std_ulogic_vector(127 downto 0);
   
 begin
 
 process1: process (clk_i, reset_i) is
+variable data  : std_ulogic_vector((2**register_select_size_g)*register_width_g-1 downto 0);  
 variable address_X : integer;
 variable address_Y : integer;
 variable write_address : integer;
 begin
     if (reset_i = '1') then               -- asynchronous reset
-        data <= (others => '0');
+        data := (others => '0');
 	end if;
-	if (clk_i'event and clk_i = '0') then  -- writing with falling clock edge 
+	if (rising_edge(clk_i)) then
 		if(write_enable_i = '1') then
 			write_address := to_integer(unsigned(write_address_i));
-			data((write_address+1)*register_width_g-1 downto write_address*register_width_g) <= write_data_i;
+			data((write_address+1)*register_width_g-1 downto write_address*register_width_g) := write_data_i;
 		end if;
-	end if;
-	if (clk_i'event and clk_i = '1') then -- reading with rising clock edge 
 		address_X := to_integer(unsigned(read_X_address_i));
 		address_Y := to_integer(unsigned(read_Y_address_i));
 		read_X_data_o <= data((address_X+1)*register_width_g-1 downto address_X*register_width_g);
 		read_Y_data_o <= data((address_Y+1)*register_width_g-1 downto address_Y*register_width_g);
-    end if;
+     end if;
+	
 end process process1;
 
 end Behavioral;
