158317af1cbc8f9d2532c2959e0ec599eca8b78c
Jochen Benzenhöfer <foulerLP@gmail.com>
2021-12-01 18:48:23 +0100

    IO v1 (basic implementation)

--- VHDL/IO.vhd
+++ VHDL/IO.vhd
@@ -21,6 +21,7 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,18 +33,33 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity IO is
-    Port ( port_id_i : in STD_LOGIC;
-           value_i : in STD_LOGIC;
-           in_out_i : in STD_LOGIC;
-           enable_i : in STD_LOGIC;
-           value_o : out STD_LOGIC;
-           clk_i : in STD_LOGIC;
-           port_b : inout STD_LOGIC_VECTOR (256 downto 0));
+    Port ( port_id_i : in std_ulogic_vector(7 downto 0);
+           value_i : in std_ulogic_vector(7 downto 0);
+           in_out_i : in std_ulogic_vector(7 downto 0);
+           enable_i : in std_ulogic;
+           value_o : out std_ulogic_vector(7 downto 0);
+           clk_i : in std_ulogic;
+           port_b : inout std_ulogic_vector (256 downto 0));
 end IO;
 
 architecture Behavioral of IO is
-
 begin
-
-
+    process(clk_i)
+    begin
+        if rising_edge(clk_i) and enable_i = '1' then
+           if in_out_i = "0" then
+            for i in 0 to 7 loop 
+                value_o(i) <= port_b(to_integer(unsigned(port_id_i)) + i);
+            end loop;
+           else 
+            for i in 0 to 7 loop 
+                port_b(i) <= value_i(to_integer(unsigned(port_id_i)) + i);
+            end loop;
+           end if;
+        elsif rising_edge(clk_i) and enable_i = '0' then
+            for i in 0 to 256 loop 
+                port_b(i) <= '0';
+            end loop;
+        end if;        
+    end process;
 end Behavioral;
