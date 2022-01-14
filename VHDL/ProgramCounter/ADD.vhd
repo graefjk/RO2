dab640a9b1d8eac0fde98839a59ec4dd4e7e046a
The_Elite_Knight <57091080+Elite0Knight@users.noreply.github.com>
2022-01-14 20:20:36 +0100

    added incrementer for PC

--- VHDL/ProgramCounter/ADD.vhd
+++ VHDL/ProgramCounter/ADD.vhd
@@ -2,18 +2,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity ADD is --kann überschrieben werden nur aus testgründen erstellt
+entity ADD is
     port (  sA_i : in std_ulogic_vector(11 downto 0);
-            sB_i : in std_ulogic_vector(11 downto 0);
-            reset_i : in std_ulogic;
-            clk_i : in std_ulogic;
-            sC_o : out std_ulogic_vector(11 downto 0));
+            sB_o : out std_ulogic_vector(11 downto 0));
 end ADD;
 
 architecture Behavioral of ADD is
 
-    signal tmp_s : std_ulogic_vector(11 downto 0) := "000000000000"; --temporary value of next instruction
+    signal increment : std_ulogic_vector(11 downto 0) := "000000000001";
 
 begin
+    sB_o <= to_stdulogicvector(to_stdlogicvector(sA_i) + to_stdlogicvector(increment));
 
 end Behavioral;
--- VHDL/ProgramCounter/ADD_tb.vhd
+++ VHDL/ProgramCounter/ADD_tb.vhd
@@ -35,19 +35,14 @@ end sim_ADD_tb;
 
 architecture Behavioral of sim_ADD_tb is
     component ADD
-		port (  sA_i : in std_ulogic_vector(11 downto 0);
-				sB_i : in std_ulogic_vector(11 downto 0);
-				reset_i : in std_ulogic;
-				clk_i : in std_ulogic;
-				sC_o : out std_ulogic_vector(11 downto 0));
+        port (
+            sA_i : in std_ulogic_vector(11 downto 0);
+            sB_o : out std_ulogic_vector(11 downto 0));
     end component;
 
     signal sA_s : std_ulogic_vector(11 downto 0);
-    signal sB_s : std_ulogic_vector(11 downto 0);
-    --signal enable_s : std_logic;
-    signal reset_s : std_ulogic;
     signal clk_s : std_logic;
-    signal sC_s : std_ulogic_vector(11 downto 0);
+    signal sB_s : std_ulogic_vector(11 downto 0);
 
     constant clk_period : time := 20 ns;
 
@@ -55,10 +50,7 @@ begin
 
     uut : ADD port map(
         sA_i => sA_s,
-        sB_i => sB_s,
-        reset_i => reset_s,
-        clk_i => clk_s,
-        sC_o => sC_s);
+        sB_o => sB_s);
 
     clk_process : process
     begin
@@ -71,53 +63,57 @@ begin
     stimuli : process
         variable err_cnt : integer := 0;
     begin
-        --enable_s <= '1';
-        reset_s <= '0';
-        wait for 5 ns;
-        reset_s <= '1';
-        wait for 5 ns;
-        reset_s <= '0';
-        wait for 10 ns;
 
         sA_s <= (others => '0');
         sA_s(0) <= '0';
-        sB_s <= (others => '0');
-        sB_s(0) <= '1';
         wait for 1 ns;
-        if (not(sC_s(0) = '1')) then
+        if (not(sB_s(0) = '1')) then
             err_cnt := err_cnt + 1;
-            report "0+1=1 Failed";
+            report "00+01=01 Failed";
         end if;
-        
+
         wait for 3 ns;
-        
+
         sA_s <= (others => '0');
         sA_s(0) <= '1';
-        sB_s <= (others => '0');
-        sB_s(0) <= '1';
         wait for 1 ns;
-        if (not(sC_s(1 downto 0) = "10")) then
+        if (not(sB_s(1 downto 0) = "10")) then
             err_cnt := err_cnt + 1;
-            report "1+1=1 Failed";
+            report "01+01=10 Failed";
         end if;
-        
-        reset_s <= '1';
-        wait for 1 ns;
-        reset_s <= '0';
-        wait for 1 ns;
-        
-        sA_s <= (others => '0');
-        sA_s(1 downto  0) <= "10";
-        sB_s <= (others => '0');
-        sB_s(0) <= '1';
-        wait for 1 ns;
-        if (not(sC_s(1 downto 0) = "11")) then
-            err_cnt := err_cnt + 1;
-            report "2+1=1 Failed";
-        end if;
-        
+
         wait for 3 ns;
-        
+
+        sA_s <= (others => '0');
+        sA_s(1 downto 0) <= "10";
+        wait for 1 ns;
+        if (not(sB_s(1 downto 0) = "11")) then
+            err_cnt := err_cnt + 1;
+            report "10+01=11 Failed";
+        end if;
+
+        wait for 3 ns;
+
+        sA_s <= (others => '1');
+        sA_s(1 downto 0) <= "11";
+        wait for 1 ns;
+        if (not(sB_s(11 downto 0) = "000000000000")) then
+            err_cnt := err_cnt + 1;
+            report "...111 + ...001 = ...000 Failed";
+        end if;
+
+        wait for 3 ns;
+
+        sA_s <= (others => '1');
+        sA_s(1 downto 0) <= "10";
+        wait for 1 ns;
+        if (not(sB_s(11 downto 0) = "111111111111")) then
+            err_cnt := err_cnt + 1;
+            report "...110 + ...001 = ...111 Failed";
+        end if;
+
+        wait for 3 ns;
+
         if err_cnt = 0 then
             report "Overall Test Passed";
         else
