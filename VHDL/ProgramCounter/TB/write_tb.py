1fb41001dfe7cbe44bc07fda1c6615158545f00f
SimonNass <57091081+SimonNass@users.noreply.github.com>
2022-01-28 16:44:00 +0100

    ADD_TB, start of IP_TB

--- VHDL/ProgramCounter/TB/write_tb.py
+++ VHDL/ProgramCounter/TB/write_tb.py
@@ -1,5 +1,7 @@
 import numpy
 
+#PC Test Benches
+
 f = open("PC_tb_long1.vhd", "w")
 start_template = open("template_begining.vhd", "r")
 template = open("template.vhd", "r")
@@ -98,6 +100,101 @@ for line in lines:
             f.write("wait for waitTime;\n")
         #stop case
         f.write("reset_s <= \'0\';\n")
+        f.write("wait for 100ns;\n")
+    else:
+        f.write("")
+f.close
+
+#ADD Test Benches
+
+f = open("ADD_tb_long1.vhd", "w")
+start_template = open("template_begining2.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        #reset case
+        for i in range(0, 2 ** 12):
+            bin = numpy.binary_repr(i,12)
+            f.write("sA_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            inext = i + 1
+            bin = numpy.binary_repr(inext,12)
+            f.write("assert sB_s = \"" + bin + "\" \n")
+            f.write("\t" + "report \"PC error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+        #stop case
+        f.write("wait for 100ns;\n")
+    else:
+        f.write("")
+f.close
+
+#IP Test Benches
+
+f = open("IP_tb_long1.vhd", "w")
+start_template = open("template_begining3.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        #reset case
+        for i in range(0, 2 ** 12):
+            bin = numpy.binary_repr(i,12)
+            f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            #f.write("assert sB_s = \"" + bin + "\" \n")
+            #f.write("\t" + "report \"PC error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+        #stop case
+        f.write("wait for 100ns;\n")
+    else:
+        f.write("")
+f.close
+
+#Stack Test Benches
+
+f = open("Stack_tb_long1.vhd", "w")
+start_template = open("template_begining4.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        #reset case
+        for i in range(0, 2 ** 12):
+            bin = numpy.binary_repr(i,12)
+            #f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            #f.write("assert sB_s = \"" + bin + "\" \n")
+            #f.write("\t" + "report \"PC error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+        #stop case
         f.write("wait for 100ns;\n")
     else:
         f.write("")
