192e536288c36c0fd7b3f2cdfdd054db0f937893
SimonNass <57091081+SimonNass@users.noreply.github.com>
2022-02-03 12:14:07 +0100

    Stack_TB

--- VHDL/ProgramCounter/TB/write_tb.py
+++ VHDL/ProgramCounter/TB/write_tb.py
@@ -234,7 +234,7 @@ for line in lines:
         f.write("report \"The Test has started \";\n\n")
         #vorherige Benutzung simulieren Start
         f.write("write_or_read_s <= \'0\';\n")
-        f.write("pc_s <= \"111111111111\";\n")
+        f.write("pc_s <= \"001111001111\";\n")
         f.write("wait for waitTime;\n")
         f.write("wait for waitTime;\n")
         f.write("write_or_read_s <= \'1\';\n")
@@ -246,7 +246,7 @@ for line in lines:
             bin = numpy.base_repr(i, base=2).zfill(12)
             f.write("pc_s <= \"" + bin + "\";\n")
             f.write("wait for waitTime;\n")
-            f.write("assert sStack_s = \"" + bin + "\" \n")
+            f.write("assert sStack_s = \"001111001111\" \n")
             f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
             f.write("assert full_s = \'0\' \n")
             f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
@@ -294,6 +294,187 @@ for line in lines:
             f.write("assert empty_s = \'0\' \n")
             f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
             f.write("wait for waitTime;\n")
+        f.write("report \"The Test is finished \";\n\n")
+        f.write("wait for 10ns;\n")
+    else:
+        f.write("")
+f.close
+
+#full Test laufzeit
+f = open("Stack_tb_long4.vhd", "w")
+start_template = open("template_begining_stack.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        f.write("report \"The Test has started \";\n\n")
+        #vorherige Benutzung simulieren Start
+        f.write("write_or_read_s <= \'0\';\n")
+        f.write("pc_s <= \"001111001111\";\n")
+        f.write("wait for waitTime;\n")
+        f.write("wait for waitTime;\n")
+        f.write("write_or_read_s <= \'1\';\n")
+        f.write("wait for waitTime;\n")
+        f.write("wait for waitTime;\n")
+        #vorherige Benutzung simulieren Ende
+        f.write("write_or_read_s <= \'0\';\n")
+        for i in range(0, 2 ** 7):
+            bin = numpy.base_repr(i, base=2).zfill(12)
+            f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("wait for waitTime;\n")
+        for i in range(0, maxBit12_allOne):
+            bin = numpy.base_repr(i, base=2).zfill(12)
+            f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("assert sStack_s = \"" + bin + "\" \n")
+            f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
+            f.write("assert full_s = \'1\' \n")
+            f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
+            f.write("assert empty_s = \'0\' \n")
+            f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+        f.write("report \"The Test is finished \";\n\n")
+        f.write("wait for 10ns;\n")
+    else:
+        f.write("")
+f.close
+
+#LiFo Test
+f = open("Stack_tb_long5.vhd", "w")
+start_template = open("template_begining_stack.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        f.write("report \"The Test has started \";\n\n")
+        f.write("write_or_read_s <= \'0\';\n")
+        f.write("wait for waitTime;\n")
+        for i in range(0, 2 ** 7-1):
+            bin = numpy.base_repr(i, base=2).zfill(12)
+            f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("wait for waitTime;\n")
+        #
+        f.write("wait for waitTime;\n")
+        f.write("write_or_read_s <= \'1\';\n")
+        for i in range(0, 2 ** 7-1):
+            bin = numpy.base_repr((2 ** 7-2) - i, base=2).zfill(12)
+            #f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("pc_s <= \"001111001111\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("assert sStack_s = \"" + bin + "\" \n")
+            f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
+            f.write("assert full_s = \'0\' \n")
+            f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
+            f.write("assert empty_s = \'0\' \n")
+            f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+        f.write("report \"The Test is finished \";\n\n")
+        f.write("wait for 10ns;\n")
+    else:
+        f.write("")
+f.close
+
+#standart Durchlauf
+f = open("Stack_tb_long6.vhd", "w")
+start_template = open("template_begining_stack.vhd", "r")
+template = open("template.vhd", "r")
+end_template = open("template_end.vhd", "r")
+lines = template.readlines()
+linesstart = start_template.readlines()
+linesend = end_template.readlines()
+for line in lines:
+    if line == "--insert_start_here\n":
+        for lines in linesstart:
+            f.write(lines)
+    elif line == "--insert_end_here\n":
+        for linee in linesend:
+            	f.write(linee)
+    elif line == "--insert_code_here\n":
+        f.write("report \"The Test has started \";\n\n")
+        #vorherige Benutzung simulieren Start
+        f.write("write_or_read_s <= \'0\';\n")
+        f.write("pc_s <= \"001111001111\";\n")
+        f.write("wait for waitTime;\n")
+        f.write("wait for waitTime;\n")
+        f.write("write_or_read_s <= \'1\';\n")
+        f.write("wait for waitTime;\n")
+        f.write("wait for waitTime;\n")
+        #vorherige Benutzung simulieren Ende
+        f.write("write_or_read_s <= \'0\';\n")
+        f.write("wait for waitTime;\n")
+        f.write("wait for 5ns;\n")
+        for i in range(0, maxBit12_allOne):
+            f.write("write_or_read_s <= \'0\';\n")
+            for j in range(0, 2):
+                bin = numpy.base_repr(j, base=2).zfill(12)
+                f.write("pc_s <= \"" + bin + "\";\n")
+                f.write("wait for waitTime;\n")
+                f.write("wait for waitTime;\n")
+            bin = numpy.base_repr(i, base=2).zfill(12)
+            f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("wait for waitTime;\n")
+            for j in range(0, 2):
+                bin = numpy.base_repr(j, base=2).zfill(12)
+                f.write("pc_s <= \"" + bin + "\";\n")
+                f.write("wait for waitTime;\n")
+                f.write("wait for waitTime;\n")
+            f.write("write_or_read_s <= \'1\';\n")
+            for j in range(0, 2):
+                bin = numpy.base_repr(1-j, base=2).zfill(12)
+                #f.write("pc_s <= \"" + bin + "\";\n")
+                f.write("pc_s <= \"001111001111\";\n")
+                f.write("wait for waitTime;\n")
+                f.write("assert sStack_s = \"" + bin + "\" \n")
+                f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
+                f.write("assert full_s = \'0\' \n")
+                f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
+                f.write("assert empty_s = \'0\' \n")
+                f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
+                f.write("wait for waitTime;\n")
+            bin = numpy.base_repr(i, base=2).zfill(12)
+            #f.write("pc_s <= \"" + bin + "\";\n")
+            f.write("pc_s <= \"001111001111\";\n")
+            f.write("wait for waitTime;\n")
+            f.write("assert sStack_s = \"" + bin + "\" \n")
+            f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
+            f.write("assert full_s = \'0\' \n")
+            f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
+            f.write("assert empty_s = \'0\' \n")
+            f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
+            f.write("wait for waitTime;\n")
+            for j in range(0, 2):
+                bin = numpy.base_repr(1-j, base=2).zfill(12)
+                #f.write("pc_s <= \"" + bin + "\";\n")
+                f.write("pc_s <= \"001111001111\";\n")
+                f.write("wait for waitTime;\n")
+                f.write("assert sStack_s = \"" + bin + "\" \n")
+                f.write("\t" + "report \"Stack error at " + bin + "\" severity error;\n\n")
+                f.write("assert full_s = \'0\' \n")
+                f.write("\t" + "report \"StackFull error at " + bin + "\" severity error;\n\n")
+                f.write("assert empty_s = \'0\' \n")
+                f.write("\t" + "report \"StackEmpty error at " + bin + "\" severity error;\n\n")
+                f.write("wait for waitTime;\n")
         f.write("report \"The Test is finished \";\n\n")
         f.write("wait for 10ns;\n")
     else:
